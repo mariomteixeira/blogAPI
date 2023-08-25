ae39e79bd884aae6bd90bda57c1d4303769c63ac
mariomteixeira <mariomarcio12324@gmail.com>
2023-08-25 15:27:37 -0300

    Inicio do projeto Blogs API.

--- src/app.js
+++ src/app.js
@@ -12,6 +12,7 @@ app.get('/', (_request, response) => {
 app.use(express.json());
 
 // ...
+// Incio do projeto.
 
 // É importante exportar a constante `app`,
 // para que possa ser utilizada pelo arquivo `src/server.js`
